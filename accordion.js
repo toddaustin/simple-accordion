c8bbf5fe76a1351c96435477e70aad00e6c2ec7d
Todd Austin <toddaustin@users.noreply.github.com>
2016-05-27 15:27:21 -0700

    Create accordion.js

---
+++ accordion.js
@@ -0,0 +1,16 @@
+ 
+var allAccordion = $('.accordion > div').hide();
+
+$('.accordion > p').click(function() {
+  if ( $(this).next().is(':visible') ) {
+    allAccordion.hide('fast');
+    return false;
+  } else {
+    //close accordion
+    allAccordion.hide('fast');
+}
+
+//open accordion
+$(this).next().slideDown("slow");
+  return false;
+});
